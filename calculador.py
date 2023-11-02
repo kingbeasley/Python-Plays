5c1e1f0546c7a265994a0e93a8556b8807ea6f01
Ruben <147420257+kingbeasley@users.noreply.github.com>
2023-11-02 16:12:27 +0100

    Basic Python Calculator

---
+++ calculador.py
@@ -0,0 +1,9 @@
+def suma_numeros(num1, num2):
+    return num1 + num2
+
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+resultado = suma_numeros(numero1, numero2)
+
+print(f"La suma de {numero1} y {numero2} es: {resultado}")
